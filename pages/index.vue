41a2603645c6bb7f7b44c725969427a03f57a019
KhalilGazairly <khalil.gazairly@gmail.com>
2022-06-14 23:21:12 +0200

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,6 @@
 
 <script>
 import Index from '../components/index/index.vue'
-<<<<<<< HEAD
-// import Login from '../components/login/login.vue'
-=======
->>>>>>> b933a5c9376eb76f2e7192d57a34c7c4623cf7c1
 export default {
    data: () => ({
 

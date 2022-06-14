96c051c93aaa31bf830ee816826c2bcf81bcd788
KhalilGazairly <khalil.gazairly@gmail.com>
2022-06-14 22:25:56 +0200

    first commit

--- pages/Notifications/_id.vue
+++ pages/Notifications/_id.vue
@@ -1,116 +1,109 @@
 <template>
-  <v-row>
-    
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="6">
-                <v-text-field
-                  :label="$t('Name')"
-                  prefix=""
-                  outlined
-                  v-model="userEdit.name"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="6">
-                <v-text-field
-                  :label="$t('Email')"
-                  v-model="userEdit.email"
-                  required
-                  outlined
-                ></v-text-field>
-              </v-col>
-              <v-col cols="6">
-               <v-checkbox v-if="!userEdit.is_clicked"
-                v-model="userEdit.is_clicked"
-                label="is clicked ?"
-              ></v-checkbox>
-              <v-checkbox v-else
-                v-model="userEdit.is_clicked"
-                true
-                label="is clicked ?"
-                
-              ></v-checkbox>
-              </v-col>
-               
-            </v-row>
-          </v-container>
-        </v-card-text>
-
-          <v-btn color="blue darken-1" text @click="UpdateUserDetail">
-            {{$t('Update')}}
-          </v-btn>
-  </v-row>
+  <div class="mt-4" v-if="allnotificationList.loading">
+    <LoadingP />
+  </div>
+  <v-form v-else>
+    <v-text-field
+      v-model="allnotificationList.oneNotification.subject"
+      :counter="10"
+      label="subject"
+      required
+      outlined
+    ></v-text-field>
+    <v-textarea
+      outlined
+      v-model="allnotificationList.oneNotification.content"
+      label="content"
+      :value="allnotificationList.oneNotification.content"
+    ></v-textarea>
+    <v-checkbox
+      v-if="!allnotificationList.oneNotification.is_clicked"
+      v-model="allnotificationList.oneNotification.is_clicked"
+      label="is clicked ?"
+    ></v-checkbox>
+    <v-checkbox
+      v-else
+      v-model="allnotificationList.oneNotification.is_clicked"
+      true
+      label="is clicked ?"
+    ></v-checkbox>
+    <v-btn color="success" class="sub" @click="changenotification">
+      submit
+    </v-btn>
+  </v-form>
 </template>
+
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
-
+import LoadingP from '../../components/tools/loadingP.vue'
+import NoData from '../../components/tools/no-data.vue'
 export default {
-  data: () => ({
-    errorMessages: '',
-    name: null,
-
-    formHasErrors: false,
-    dialog: false,
-    hidden: false,
-    panel: [0, 1],
-
-    userEdit: {
-      name: '',
-      email: '',
-      is_clicked:false,
-      id:''
-    },
-    UserEmail: null,
-  }),
-  computed: {
-    form() {
-      return {
-        name: this.name,
-        address: this.address,
-        city: this.city,
-        state: this.state,
-        zip: this.zip,
-        country: this.country,
-      }
-    },
+  name: 'IndexPage',
+  data() {
+    return {
+      datanotification: {
+        content: '',
+        subject: '',
+        is_clicked: false,
+        id: this.$route.params.id,
+      },
+    }
   },
-
-  watch: {
-    name() {
-      this.errorMessages = ''
-    },
+  head() {
+    return {
+      title: this.$i18n.t('Subcategories'),
+    }
   },
-
-  methods: {
-    ...mapActions(['updateNotification','getoneNotification']),
-
-    completeUserData() {
-      this.userEdit.name = this.allnotificationList.oneNotification.subject
-      this.userEdit.email = this.allnotificationList.oneNotification.content
-      this.userEdit.is_clicked = this.allnotificationList.oneNotification.is_clicked
-      this.userEdit.id = this.$route.params.id
-    },
-    UpdateUserDetail() {
-      console.log(this.userEdit)
-      this.updateNotification(this.userEdit);
-    },
+  components: {
+    LoadingP,
+    NoData,
   },
-
   computed: {
     ...mapGetters(['allnotificationList']),
   },
-
-
+  methods: {
+    ...mapActions(['updateNotification', 'getoneNotification']),
+    changenotification(e) {
+      e.preventDefault()
+      alert(this.datanotification.content)
+      console.log(this.datanotification)
+      this.updateNotification(this.datanotification)
+    },
+  },
   mounted() {
     this.getoneNotification(this.$route.params.id)
-    this.completeUserData()
   },
 }
 </script>
 
+<style scoped>
+.subCat_ {
+  border: 1px solid #ededed;
+  border-radius: 4px;
+  padding: 25px;
+}
 
-<style>
+.subCategorie_ {
+  border-radius: 4px;
+  box-shadow: 0px 1px 4px 0 rgb(0 0 0 / 5%);
+  padding-top: 20px;
+}
+.subCategorie_:hover {
+  transition: 0.5ms;
+  box-shadow: 0px 0px 7px 0px rgb(71 71 71 / 26%);
+}
+.subCategorie_ .v-image {
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  border: 1px solid #f2f2f2cc;
+  margin: auto;
+  margin-bottom: 20px;
+}
+.subCategorie_ h3 {
+  padding: 14px;
+  background: #fff;
+  border-top: 1px solid #f7f7f7;
+}
 </style>
